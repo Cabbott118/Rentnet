f695e8660b703d43ac465861b13522cc693db0a5
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-08-05 13:56:47 -0400

    Trying fix for production mode not finding image path.

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,7 +4,7 @@ const path = require('path');
 // Set up Multer Storage Engine
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads');
+    cb(null, './client/src/uploads');
   },
   filename: (req, file, cb) => {
     cb(
